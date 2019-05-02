9e8282e2b718c2a4cb042f1d50d4b21cded8c262
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-05-01 19:56:44 -0700

    Use os.path.join; DAT is data root.

--- cfgnlog/Configure.py
+++ cfgnlog/Configure.py
@@ -5,65 +5,67 @@
 import os, json
 from Fun import env, mkdirs, die
 import Defaults as DEFAULT
-from Defaults import DAT, CACHE
+from Defaults import APP_NAME, DAT, CACHE
 
 # XDG Specification Compliance # <https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html>
 DEFAULT_MODE = 0o700
+ENV_HOME = env( 'HOME' )
 CONFIGURATION_DIRECTORY_XDG = 'XDG_CONFIG_HOME'
-CONFIGURATION_DIRECTORY_XDG_DEF = env( 'HOME' ) + '/.config' #TODO: use os.path.join
+CONFIGURATION_DIRECTORY_XDG_DEF = os.path.join( ENV_HOME, '.config' )
 DATA_DIRECTORY_XDG = 'XDG_DATA_HOME'
-DATA_DIRECTORY_XDG_DEF =  env( 'HOME' ) + '/.local/share' #TODO: use os.path.join
+DATA_DIRECTORY_XDG_DEF =  os.path.join( ENV_HOME,  '.local', 'share' )
 CACHE_DIRECTORY_XDG = 'XDG_CACHE_HOME'
-CACHE_DIRECTORY_XDG_DEF = env( 'HOME' ) + '/.cache'
+CACHE_DIRECTORY_XDG_DEF = os.path.join( ENV_HOME, '.cache' )
+
+# LOG_FILENAME = APP_NAME + '.log' #TODO
 
 # Windows Compatibility #
 CONFIGURATION_DIRECTORY_WIN = 'LOCALAPPDATA'	#Except XP
 
 class Configure(  ):
-	def __init__( self, config_file = None ):
-		#Application is responsible for creation of DAT and CACHE directory
+	def __init__( self, config_file = None, IOErrorCallback = die ):
+		# Application is responsible for just in time creation of DAT and CACHE directories
 		if( config_file == None ):
 			config_file = env( CONFIGURATION_DIRECTORY_XDG )
 			if( config_file == '' ): config_file = env( CONFIGURATION_DIRECTORY_WIN );
 			if( config_file == '' ): config_file = CONFIGURATION_DIRECTORY_XDG_DEF;
-			config_file += DEFAULT.CONFIGURATION_DIRECTORY_NAME
+			config_file = os.path.join( config_file, APP_NAME )
 			if not os.path.exists( config_file ): mkdirs( config_file, DEFAULT_MODE, 'Creating configuration directory' );
-			config_file += DEFAULT.CONFIGURATION_FILENAME
+			config_file = os.path.join( config_file, DEFAULT.CONFIGURATION_FILENAME )
 		self.dirty = self.load( config_file )
 		if( DAT not in self.param ):
 			self.param[DAT] = env( DATA_DIRECTORY_XDG )
 			if( self.param[DAT] == '' ): self.param[DAT] = env( CONFIGURATION_DIRECTORY_WIN );
 			if( self.param[DAT] == '' ): self.param[DAT] = DATA_DIRECTORY_XDG_DEF;
-			self.param[DAT] += DEFAULT.DATA_DIRECTORY_NAME
+			self.param[DAT] = os.path.join( self.param[DAT], APP_NAME )
 			self.dirty = True
 		if( CACHE not in self.param ):
 			self.param[CACHE] = env( CACHE_DIRECTORY_XDG )
-			if( self.param[CACHE] == '' ): self.param[CACHE] = env( CONFIGURATION_DIRECTORY_WIN )
-			if( self.param[CACHE] == '' ): self.param[CACHE] = CACHE_DIRECTORY_XDG_DEF + DEFAULT.CACHE_DIRECTORY_NAME;
-			else: self.param[CACHE] += DEFAULT.CACHE_DIRECTORY_NAME + 'cache' + os.sep#TODO: use os.path.join
+			if( self.param[CACHE] == '' ): self.param[CACHE] = env( CONFIGURATION_DIRECTORY_WIN );
+			if( self.param[CACHE] == '' ): self.param[CACHE] = os.path.join( CACHE_DIRECTORY_XDG_DEF, APP_NAME );
+			else: self.param[CACHE] = os.path.join( self.param[CACHE], APP_NAME, 'cache' ); #WINDOWS
 			self.dirty = True
 		for key in DEFAULT.CONFIGURATION_PARAMETERS:
 			if( key not in self.param ):
 				self.param[key] = DEFAULT.CONFIGURATION_PARAMETERS[key]
 				self.dirty = True
-		if( self.dirty ): self.update( config_file );
-		self.f_ref = config_file #XXX
-	def update( self, config_file ):
+		if( self.dirty ): self.update( IOErrorCallback );
+	def update( self, IOErrorCallback = die ):
 		try:
-			fh = open( config_file, 'w', DEFAULT_MODE )
+			fh = open( self.config_file, 'w', DEFAULT_MODE )
 			fh.write( json.dumps( self.param ) ) #TODO: Only write necessary parts
 			fh.close(  )
 			self.dirty = False
-		except IOError as e: die( e ); #TODO: Ask user to select a file. Delegate up.
-	def load( self, config_file ): # Returns true if needs (re)write.
+		except IOError as e: IOErrorCallback( e );
+	def load( self, config_file, param = DEFAULT.CONFIGURATION_PARAMETERS ): # Returns true if needs (re)write.
+		self.config_file = config_file
 		try:
 			fh = open( config_file )
 			self.param = json.loads( fh.read(  ) )
 			fh.close(  )
-			#TODO: verify version and update when needed
 			return False
 		except IOError:
-			self.param = DEFAULT.CONFIGURATION_PARAMETERS
+			self.param = param
 			print( 'Creating configuration file: ' + config_file )
 			return True
 		#TODO: except ValueError: malformed configuration file
--- cfgnlog/Defaults.py
+++ cfgnlog/Defaults.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
 
-import os
 
 APP_NAME = 'Ymir'
 
 VERSION = [0,0,0] #TODO: Begin incrementing once backwards compabibility is needed.
 
 CONFIGURATION_FILENAME = 'settings.cfg'
-CONFIGURATION_DIRECTORY_NAME = os.sep + APP_NAME + os.sep
-DATA_DIRECTORY_NAME = os.sep + os.path.join( APP_NAME, 'Contacts' ) + os.sep
-CACHE_DIRECTORY_NAME = os.sep + APP_NAME + os.sep
-LOG_FILENAME = APP_NAME + '.log' #TODO
 
 DAT = 'DAT'
 CACHE = 'CACHE'
@@ -18,4 +13,4 @@ CACHE = 'CACHE'
 CONFIGURATION_PARAMETERS = {
 	'V' : VERSION
 }
-#NOTE: Dynamically Included Default Configuration Parameters: { DATA_DIR, 'REV' } TODO: LOG
+#NOTE: Dynamically Included Default Configuration Parameters: { DAT, CACHE } TODO: LOG, 'REV'?
