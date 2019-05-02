f508e59f45fe9f5bfef4943b45d11a38956699da
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-05-01 19:29:57 -0700

    Minor TODO cleanup.

--- cfgnlog/Defaults.py
+++ cfgnlog/Defaults.py
@@ -4,11 +4,13 @@ APP_NAME = 'Ymir'
 
 VERSION = [0,0,0] #TODO: Begin incrementing once backwards compabibility is needed.
 
-CONFIGURATION_FILENAME = 'config.txt' #XXX
+CONFIGURATION_FILENAME = 'settings.cfg'
 DATA_DIRECTORY_NAME = '/' + APP_NAME + '/Data/'
-#TODO: LOG_FILENAME = APP_NAME + '.log' #XXX
+LOG_FILENAME = APP_NAME + '.log' #TODO
 
 DAT = 'DAT'
 
-CONFIGURATION_PARAMETERS = { 'V' : VERSION }
-#NOTE: Dynamically Included Default Configuration Parameters: { DATA_DIR, 'REV' }
+CONFIGURATION_PARAMETERS = {
+	'V' : VERSION
+}
+#NOTE: Dynamically Included Default Configuration Parameters: { DATA_DIR, 'REV' } TODO: LOG
